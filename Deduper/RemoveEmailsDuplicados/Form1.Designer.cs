c206964c52852f78ee5495a804045ab6c152a419
Thais Maria <thais.marpi@gmail.com>
2019-12-10 09:20:06 -0300

    Resolvido problema da progress bar adiantada

--- Deduper/RemoveEmailsDuplicados/Form1.Designer.cs
+++ Deduper/RemoveEmailsDuplicados/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.progressBar1.Location = new System.Drawing.Point(206, 273);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(261, 23);
+            this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 26;
             // 
             // LabelApagados
